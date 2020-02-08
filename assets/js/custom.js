abbc3b84d3bc69b182fcc743bdc479fbae7c14fa
siyathokoza <siyathokozacmbatha@gmail.com>
2020-02-08 13:25:16 +0200

    updated nav dropdown menus

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+document.querySelector('#solutions').addEventListener('click',(e)=>{
+    link =  e.target.href;
+    document.location = link;
+});
+
+document.querySelector('#industries').addEventListener('click',(e)=>{
+    link =  e.target.href;
+    document.location = link;
+});
